c0a20bb9001bada4eac92d608ffd7eda2ef837e6
Angel Ivory D <116862323+Moonarii@users.noreply.github.com>
2024-11-08 22:27:12 +0800

    Update and rename test.sql to data.sql

--- mysystem/sql/data.sql
+++ mysystem/sql/data.sql
@@ -18,3 +18,10 @@ CREATE TABLE projects (
 );
 
 
+CREATE TABLE user_passwords (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50),
+  password VARCHAR(50),
+  date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  
+);
